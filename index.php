cbdcad1b88265931ffbc02ecd092bb41e696b35c
Gabriel Solano <sgabriel67278@gmail.com>
2026-09-21 23:41:47 +0000

    Se implemento el archivo principal de acceso al sistema

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+// Definimos los datos básicos que utilizarán los layouts de esta página
+$tituloPagina = 'Inicio';
+$rutaBase = '';
+
+require_once __DIR__ . '/views/layouts/header.php';
+require_once __DIR__ . '/views/layouts/navbar.php';
+require_once __DIR__ . '/views/inicio.php';
+require_once __DIR__ . '/views/layouts/footer.php';
